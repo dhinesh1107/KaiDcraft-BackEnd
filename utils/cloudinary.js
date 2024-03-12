c164a7aa813658e0c66bf245cf3379621a13de9a
Dhinesh <81690273+dhinesh1107@users.noreply.github.com>
2024-03-12 13:44:08 +0530

    Update cloudinary

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -5,8 +5,7 @@ dotenv.config();
 cloudinary.config({ 
     cloud_name: process.env.CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET, 
-    //cloudinary_url: process.env.CLOUDINARY_URL
+    api_secret: process.env.CLOUDINARY_API_SECRET,
   });
 
 export const cloudinaryUploadImg = async(fileToUploads) => {
